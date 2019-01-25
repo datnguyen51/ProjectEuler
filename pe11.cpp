2e267924e07c8cde893f88cc3e4f830d68f555dd
Dat Nguyen <38597890+datnguyen51@users.noreply.github.com>
2019-01-26 00:45:36 +0700

    ProEuler

--- pe11.cpp
+++ pe11.cpp
@@ -5,42 +5,70 @@ long int Diagonally()
 {
 	long int mult;
 	long int max = 0;
+
+	//cheo chinh
 	for(int i=0; i<16; i++)
 	{
 		mult = 1;
 		for(int j=0; j<4; j++)
-		{
 			mult =  mult * a[i+j][i+j];
-			if(mult>max)
-				max = mult;
-		}
+		if(mult>max)
+			max = mult;
 	}
-	for(int i=0; i<16; i++)
+
+	//cheo phu
+	for(int i=19; i>2; i--)
 	{
-		for(int j=18; j>=3; j--)
+		mult = 1;
+		for(int k=0; k<4; k++)
+			mult =  mult * a[19-(i-k)][i-k];
+		if(mult>max)
+			max = mult;
+	}
+
+	//cheo duoi phai
+	for(int i=1; i<17; i++)
+		for(int j=19; j>2+i; j--)
 		{
 			mult = 1;
 			for(int k=0; k<4; k++)
-			{
-				mult =  mult * a[i+1][j-k];
-				// if(mult>max)
-					max = mult;
-			}
+				mult =  mult * a[19-(j-k)+i][j-k];
+			if(mult>max)
+				max = mult;
 		}
-	}
-	// for(int i=0; i<20; i++)
-	// {
-	// 	for(int j=1; j; j--)
-	// 	{
-	// 		mult = 1;
-	// 		for(int k=0; k<4; k++)
-	// 		{
-	// 			mult =  mult * a[i+k][j-k];
-	// 			if(mult>max)
-	// 				max = mult;
-	// 		}
-	// 	}
-	// }
+
+	//cheo tren trai
+	for(int j=18; j>2; j--)
+		for(int i=0; i<j-2; i++)
+		{
+			mult = 1;
+			for(int k=0; k<4; k++)
+				mult =  mult * a[k+i][j-i-k];
+			if(mult>max)
+				max = mult;
+		}
+
+	//cheo duoi trai
+	for(int i=1; i<17; i++)
+		for(int j=0; j<17-i; j++)
+		{
+			mult = 1;
+			for(int k=0; k<4; k++)
+				mult =  mult * a[k+i+j][j+k];
+			if(mult>max)
+				max = mult;
+		}
+
+	//chep tren phai
+	for(int j=1; j<17; j++)
+		for(int i=0; i<17-j; i++)
+		{
+			mult = 1;
+			for(int k=0; k<4; k++)
+				mult =  mult * a[k+i][j+k+i];
+			if(mult>max)
+				max = mult;
+		}
 	return max;
 }
 long int Row()
